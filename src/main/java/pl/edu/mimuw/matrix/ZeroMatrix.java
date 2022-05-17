0c339dd494e8982b702162fe9132fa60e30d20ed
Patryk Malcher <pm.patrykmalcher@gmail.com>
2022-05-17 21:39:00 +0200

    Added times method in ZeroMatrix.

--- src/main/java/pl/edu/mimuw/matrix/ZeroMatrix.java
+++ src/main/java/pl/edu/mimuw/matrix/ZeroMatrix.java
@@ -4,4 +4,16 @@ public class ZeroMatrix extends ConstMatrix {
   public ZeroMatrix(Shape shape) {
     super(shape, 0); 
   }
+
+  @Override
+  public IDoubleMatrix times(IDoubleMatrix other) {
+    Shape shapeOther = other.shape();
+
+    assert(shape().columns == shapeOther.rows): "Invalid dimensions.";
+
+    int a = shape().rows;
+    int b = shapeOther.columns; 
+
+    return new ZeroMatrix(Shape.matrix(a, b));
+  }  
 }
